3c5f72c238bb7492062178e3cf1523081abc369b
Leoodaviid <david_ax7@hotmail.com>
2023-01-10 15:42:08 -0300

    ajustes finais

--- src/pages/Sobre/Sobre.tsx
+++ src/pages/Sobre/Sobre.tsx
@@ -1,12 +1,8 @@
-import Sidebar from "../../components/Aside/Aside";
-import MusicPlayer from "../../components/MusicPlayer/MusicPlayer";
 import SectionSobre from "../../components/SectionSobre/sectionSobre";
 
 const Sobre = () => {
   return (
     <>
-      <MusicPlayer />
-      <Sidebar />
       <SectionSobre />
     </>
   );
