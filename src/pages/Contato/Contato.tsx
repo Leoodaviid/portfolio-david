cab2c13a26d4e5088f2153f54eca246b5ff1f176
Leoodaviid <david_ax7@hotmail.com>
2023-01-10 15:33:13 -0300

    retirando component aside e musicplayer

--- src/pages/Contato/Contato.tsx
+++ src/pages/Contato/Contato.tsx
@@ -1,13 +1,8 @@
-import React from "react";
-import Sidebar from "../../components/Aside/Aside";
-import MusicPlayer from "../../components/MusicPlayer/MusicPlayer";
 import SectionContato from "../../components/SectionContato/sectionContato";
 
 const Contato = () => {
   return (
     <div>
-      <MusicPlayer />
-      <Sidebar />
       <SectionContato />
     </div>
   );
