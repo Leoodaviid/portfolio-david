eb9980c1808eecae35b2bd11ea6a9fee86719805
Leoodaviid <david_ax7@hotmail.com>
2023-01-10 15:30:04 -0300

    ajustes na responsividade mobile

--- src/components/SectionSobre/style.ts
+++ src/components/SectionSobre/style.ts
@@ -25,40 +25,40 @@ const SectionSobreStyle = styled.section`
     padding: 0.5rem 0;
   }
   .profile-wrapper .btn {
-    margin: 10px 10px 0px 0px;
+    margin: 0.6rem 0.6rem 0 0;
   }
 
   .profile-wrapper .btn i {
     vertical-align: middle;
-    margin-right: 5px;
+    margin-right: 0.3rem;
   }
 
   .about-profile {
     position: relative;
-    margin: 20px 0 20px 0;
+    margin: 1.25rem 0 1.25rem 0;
   }
 
   .about-profile .pro-title {
     font-weight: 700;
-    color: #666565;
+    color: #e1e1e1;
     position: relative;
     width: 120px;
     display: inline-block;
-    margin-bottom: 5px;
+    margin-bottom: 0.3rem;
   }
 
   .about-profile .pro-title:after {
     position: absolute;
     content: ":";
-    color: #666565;
-    font-size: 14px;
-    left: 77px;
+    color: #f50057;
+    font-size: 0.8rem;
+    left: 4.8rem;
   }
 
   .about-profile .admin-sign {
     position: absolute;
-    right: 45px;
-    bottom: 10px;
+    right: 2.8rem;
+    bottom: 0.6rem;
   }
 
   @media (max-width: 800px) {
