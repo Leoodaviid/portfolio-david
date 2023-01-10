fe43603b4bd80a5d7011a4e17a6389d939fc77ec
Leoodaviid <david_ax7@hotmail.com>
2023-01-10 15:43:11 -0300

    images para pages projetos

---
+++ src/pages/Habilidades/Habilidades.tsx
@@ -0,0 +1,11 @@
+import SectionHabilidades from "../../components/SectionHabilidades/SectionHabilidades";
+
+const Habilidades = () => {
+  return (
+    <>
+      <SectionHabilidades />
+    </>
+  );
+};
+
+export default Habilidades;
