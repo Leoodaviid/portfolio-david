ec088a22ff2c4066bf0738810082b76fcd2b481d
Leoodaviid <david_ax7@hotmail.com>
2023-01-10 15:35:01 -0300

    restirando components sidebar e musicplayer

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,8 @@
-import Sidebar from "../../components/Aside/Aside";
-import MusicPlayer from "../../components/MusicPlayer/MusicPlayer";
 import SectionHome from "../../components/SectionHome/sectionHome";
 
 const Home = () => {
   return (
     <main>
-      <MusicPlayer />
-      <Sidebar />
       <SectionHome />
     </main>
   );
