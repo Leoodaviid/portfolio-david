7b2a5599c496e2881b680ed703aa8a2ee2cae788
Leoodaviid <david_ax7@hotmail.com>
2023-02-20 13:42:53 -0300

    Passando componente na page projetos

--- src/pages/Projetos/Projetos.tsx
+++ src/pages/Projetos/Projetos.tsx
@@ -1,5 +1,5 @@
-import { FaEye, FaLink } from "react-icons/fa";
 import { Section } from "./styles";
+import Agenda from "../../assets/images/beleza-agenda.png";
 import Caravan from "../../assets/images/caravan.png";
 import Dogs from "../../assets/images/dogs.png";
 import Dindin from "../../assets/images/dindin.png";
@@ -7,6 +7,7 @@ import LaParrilla from "../../assets/images/Parrilla-food-commerce.png";
 import Contador from "../../assets/images/contador.png";
 import Compras from "../../assets/images/lista-compras.png";
 import Media from "../../assets/images/media.png";
+import Projeto from "../../components/Projeto/Projeto";
 
 const Projetos = () => {
   return (
@@ -17,224 +18,60 @@ const Projetos = () => {
           <div className="row">
             <div className="col-md-12"></div>
             <div id="portfolio" className="row ">
-              <div className="col-sm-6 col-md-4 col-lg-4 col-xl-4 mix development print">
-                <div className="portfolio-item">
-                  <div className="shot-item">
-                    <img src={Caravan} alt="Interface WebSite Caravan" />
-                    <div className="overlay">
-                      <div className="icons">
-                        <a
-                          className="lightbox preview"
-                          href="https://leoodaviid.github.io/Caravan/"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <i className="icon-eye">
-                            <FaEye />
-                          </i>
-                        </a>
-                        <a
-                          className="link"
-                          href="https://github.com/Leoodaviid/Caravan"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <i className="icon-link">
-                            <FaLink />
-                          </i>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-6 col-md-4 col-lg-4 col-xl-4 mix design print">
-                <div className="portfolio-item">
-                  <div className="shot-item">
-                    <img src={Dogs} alt="Interface WebSite Dogs" />
-                    <div className="overlay">
-                      <div className="icons">
-                        <a
-                          className="lightbox preview"
-                          href="https://dog-leoodaviid.vercel.app/"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <i className="icon-eye">
-                            <FaEye />
-                          </i>
-                        </a>
-                        <a
-                          className="link"
-                          href="https://github.com/Leoodaviid/dog"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <i className="icon-link">
-                            <FaLink />
-                          </i>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-6 col-md-4 col-lg-4 col-xl-4 mix development">
-                <div className="portfolio-item">
-                  <div className="shot-item">
-                    <img
-                      src={LaParrilla}
-                      alt="Interface LaParrilla-food-commerce"
-                    />
-                    <div className="overlay">
-                      <div className="icons">
-                        <a
-                          className="lightbox preview"
-                          href=" https://github.com/Leoodaviid/laparrilla"
-                          target="_blank"
-                        >
-                          <i className="icon-eye">
-                            <FaEye />
-                          </i>
-                        </a>
-                        <a
-                          className="link"
-                          href="https://github.com/Leoodaviid/laparrilla"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <i className="icon-link">
-                            <FaLink />
-                          </i>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-6 col-md-4 col-lg-4 col-xl-4 mix development">
-                <div className="portfolio-item">
-                  <div className="shot-item">
-                    <img src={Dindin} alt="Interface WebSite DinDin" />
-                    <div className="overlay">
-                      <div className="icons">
-                        <a
-                          className="lightbox preview"
-                          href=" https://leoodaviid.github.io/DinDin/"
-                          target="_blank"
-                        >
-                          <i className="icon-eye">
-                            <FaEye />
-                          </i>
-                        </a>
-                        <a
-                          className="link"
-                          href="https://github.com/Leoodaviid/DinDin"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <i className="icon-link">
-                            <FaLink />
-                          </i>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-6 col-md-4 col-lg-4 col-xl-4 mix development design">
-                <div className="portfolio-item">
-                  <div className="shot-item">
-                    <img src={Contador} alt="Interface Contador" />
-                    <div className="overlay">
-                      <div className="icons">
-                        <a
-                          className="lightbox preview"
-                          href="https://leoodaviid.github.io/Mini-Projeto-Contador/"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <i className="icon-eye">
-                            <FaEye />
-                          </i>
-                        </a>
-                        <a
-                          className="link"
-                          href="https://github.com/Leoodaviid/Mini-Projeto-Contador"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <i className="icon-link">
-                            <FaLink />
-                          </i>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-6 col-md-4 col-lg-4 col-xl-4 mix development">
-                <div className="portfolio-item">
-                  <div className="shot-item">
-                    <img src={Compras} alt="Interface Compras" />
-                    <div className="overlay">
-                      <div className="icons">
-                        <a
-                          className="lightbox preview"
-                          href="https://leoodaviid.github.io/Lista-Compras/"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <i className="icon-eye">
-                            <FaEye />
-                          </i>
-                        </a>
-                        <a
-                          className="link"
-                          href="https://github.com/Leoodaviid/Lista-Compras"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <i className="icon-link">
-                            <FaLink />
-                          </i>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-6 col-md-4 col-lg-4 col-xl-4 mix print design">
-                <div className="portfolio-item">
-                  <div className="shot-item">
-                    <img src={Media} alt="Interface Média" />
-                    <div className="overlay">
-                      <div className="icons">
-                        <a
-                          className="lightbox preview"
-                          href="https://leoodaviid.github.io/calculo-simples-aritimetica/"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <i className="icon-eye">
-                            <FaEye />
-                          </i>
-                        </a>
-                        <a
-                          className="link"
-                          href="https://github.com/Leoodaviid/calculo-simples-aritimetica"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <i className="icon-link">
-                            <FaLink />
-                          </i>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <Projeto
+                img={LaParrilla}
+                alt={"Interface website Laparrilla."}
+                preview={"https://github.com/Leoodaviid/laparrilla"}
+                repository={"https://github.com/Leoodaviid/laparrilla"}
+              />
+              <Projeto
+                img={Dogs}
+                alt={"Interface WebSite Dogs."}
+                preview={"https://dog-leoodaviid.vercel.app/"}
+                repository={"https://github.com/Leoodaviid/dog"}
+              />
+              <Projeto
+                img={Agenda}
+                alt={"Interface WebSite Beleza na agenda."}
+                preview={"https://agenda-testes.vercel.app/"}
+                repository={"https://github.com/Leoodaviid/beleza-na-agenda"}
+              />
+              <Projeto
+                img={Caravan}
+                alt={"Interface WebSite Caravan"}
+                preview={"https://leoodaviid.github.io/Caravan/"}
+                repository={"https://github.com/Leoodaviid/Caravan"}
+              />
+              <Projeto
+                img={Dindin}
+                alt={"Interface WebSite Dindin"}
+                preview={"https://leoodaviid.github.io/DinDin/"}
+                repository={"https://github.com/Leoodaviid/DinDin"}
+              />
+              <Projeto
+                img={Compras}
+                alt={"Interface lista de compras."}
+                preview={"https://leoodaviid.github.io/Lista-Compras/"}
+                repository={"https://github.com/Leoodaviid/Lista-Compras"}
+              />
+              <Projeto
+                img={Contador}
+                alt={"Interface contador."}
+                preview={"https://leoodaviid.github.io/Mini-Projeto-Contador/"}
+                repository={
+                  "https://github.com/Leoodaviid/Mini-Projeto-Contador"
+                }
+              />
+              <Projeto
+                img={Media}
+                alt={"Interface media simples aritmetica."}
+                preview={
+                  "https://leoodaviid.github.io/calculo-simples-aritimetica/"
+                }
+                repository={
+                  "https://github.com/Leoodaviid/calculo-simples-aritimetica"
+                }
+              />
             </div>
           </div>
         </div>
