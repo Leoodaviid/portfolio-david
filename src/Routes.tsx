14fad2c671b981142dab5dd2cad697abf2c4d5d8
Leoodaviid <david_ax7@hotmail.com>
2023-03-13 16:46:11 -0300

    Ajuste, incluindo main

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,11 +3,10 @@ import {
   Route,
   BrowserRouter,
 } from "react-router-dom";
-import Sidebar from "./components/Aside/Aside";
-import MusicPlayer from "./components/MusicPlayer/MusicPlayer";
 import Contato from "./pages/Contato/Contato";
 import Habilidades from "./pages/Habilidades/Habilidades";
 import Home from "./pages/Home/Home";
+import Main from "./pages/Main/Main";
 import Projetos from "./pages/Projetos/Projetos";
 import Sobre from "./pages/Sobre/Sobre";
 import Sucesso from "./pages/Sucesso/Sucesso";
@@ -15,15 +14,15 @@ import Sucesso from "./pages/Sucesso/Sucesso";
 export default function Routes() {
   return (
     <BrowserRouter>
-      <MusicPlayer />
-      <Sidebar />
       <WrapperRoutes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sobre" element={<Sobre />} />
-        <Route path="/habilidades" element={<Habilidades />} />
-        <Route path="/projetos" element={<Projetos />} />
-        <Route path="/contato" element={<Contato />} />
-        <Route path="/sucesso" element={<Sucesso />} />
+        <Route path="/" element={<Main />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/habilidades" element={<Habilidades />} />
+          <Route path="/projetos" element={<Projetos />} />
+          <Route path="/contato" element={<Contato />} />
+          <Route path="/sucesso" element={<Sucesso />} />
+        </Route>
       </WrapperRoutes>
     </BrowserRouter>
   );
