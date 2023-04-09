c2973d69d7e85724c37d1f14a1c7fb035ad38830
Leoodaviid <david_ax7@hotmail.com>
2023-04-09 16:52:06 -0300

    feat: new project - phanox

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -1,4 +1,5 @@
 import { ProjectsDataProps } from "../models/models";
+import Phanox from "../assets/images/phanox.png";
 import LaParrilla from "../assets/images/Parrilla-food-commerce.png";
 import Bikcraft from "../assets/images/bikcraft.png";
 import Agenda from "../assets/images/beleza-agenda.png";
@@ -7,82 +8,80 @@ import Dogs from "../assets/images/dogs.png";
 import Climate from "../assets/images/climate.png";
 import ApiGitHub from "../assets/images/api-github.png";
 import Dindin from "../assets/images/dindin.png";
-import Contador from "../assets/images/contador.png";
 import Compras from "../assets/images/lista-compras.png";
 import PingPong from "../assets/images/ping-pong.png";
-import Media from "../assets/images/media.png";
 import Metaverse from "../assets/images/metaverse.png";
 
 export const projectsData = [
   {
     id: 1,
+    img: Phanox,
+    alt: "Interface website ecommerce Phanox.",
+    preview: "https://ecommerce-phanox-tan.vercel.app",
+    repository: "https://github.com/Leoodaviid/phanox-ecommerce",
+  },
+  {
+    id: 2,
     img: LaParrilla,
     alt: "Interface website Laparrilla.",
     preview: "https://github.com/Leoodaviid/laparrilla",
     repository: "https://github.com/Leoodaviid/laparrilla",
   },
   {
-    id: 2,
+    id: 3,
     img: ApiGitHub,
     alt: "Interface WebSite api-gitHub.",
     preview: "https://api-github-beta.vercel.app",
     repository: "https://github.com/Leoodaviid/api-github",
   },
   {
-    id: 3,
+    id: 4,
     img: Climate,
     alt: "Interface WebSite climate - previsão de tempo",
     preview: "https://climate-zeta.vercel.app/",
     repository: "https://github.com/Leoodaviid/climate",
   },
   {
-    id: 4,
+    id: 5,
     img: Dogs,
     alt: "Interface WebSite Dogs.",
     preview: "https://dog-leoodaviid.vercel.app/",
     repository: "https://github.com/Leoodaviid/dog",
   },
   {
-    id: 5,
+    id: 6,
     img: Agenda,
     alt: "Interface WebSite Beleza na agenda.",
     preview: "https://agenda-testes.vercel.app/",
     repository: "https://github.com/Leoodaviid/beleza-na-agenda",
   },
   {
-    id: 6,
+    id: 7,
     img: Metaverse,
     alt: "Interface WebSite Metaverse.",
     preview: "https://metaversus-leoodaviid.vercel.app/",
     repository: "https://github.com/Leoodaviid/metaversus",
   },
   {
-    id: 7,
+    id: 8,
     img: Bikcraft,
     alt: "Interface WebSite Bikcraft.",
     preview: "https://bikcraft-leoodaviid.vercel.app",
     repository: "https://github.com/Leoodaviid/bikcraft",
   },
   {
-    id: 8,
+    id: 9,
     img: Caravan,
     alt: "Interface WebSite Caravan",
     preview: "https://leoodaviid.github.io/Caravan/",
     repository: "https://github.com/Leoodaviid/Caravan",
   },
   {
-    id: 9,
+    id: 10,
     img: Dindin,
     alt: "Interface WebSite Dindin",
     preview: "https://leoodaviid.github.io/DinDin/",
     repository: "https://github.com/Leoodaviid/DinDin",
-  },
-  {
-    id: 10,
-    img: Compras,
-    alt: "Interface lista de compras.",
-    preview: "https://leoodaviid.github.io/Lista-Compras/",
-    repository: "https://github.com/Leoodaviid/Lista-Compras",
   },
   {
     id: 11,
@@ -93,16 +92,9 @@ export const projectsData = [
   },
   {
     id: 12,
-    img: Contador,
-    alt: "Interface contador.",
-    preview: "https://leoodaviid.github.io/Mini-Projeto-Contador/",
-    repository: "https://github.com/Leoodaviid/Mini-Projeto-Contador",
-  },
-  {
-    id: 13,
-    img: Media,
-    alt: "Interface media simples aritmetica.",
-    preview: "https://leoodaviid.github.io/calculo-simples-aritimetica/",
-    repository: "https://github.com/Leoodaviid/calculo-simples-aritimetica",
+    img: Compras,
+    alt: "Interface lista de compras.",
+    preview: "https://leoodaviid.github.io/Lista-Compras/",
+    repository: "https://github.com/Leoodaviid/Lista-Compras",
   },
 ] as ProjectsDataProps[];
